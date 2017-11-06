1e203b33a59c9d13057a767fdfb7dedf9f116854
Oskar Singer <oskarsinger@gmail.com>
2017-11-06 17:26:46 -0500

    Started ROS sketching

---
+++ theline/sketching/ros.py
@@ -0,0 +1,17 @@
+import fht
+
+import numpy as np
+
+from drrobert.random import rademacher
+from theline.utils import RowPermutationMatrix as RPM
+from scipy.stats import gengamma
+
+class RandomOrthogonalSystemSketcher:
+
+    def __init__(self):
+
+        pass
+
+    def get_sketched(self, A):
+
+        pass
