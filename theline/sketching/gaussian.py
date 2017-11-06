df95a382ed73a2c70b5464698986de4ccd3e1c03
Oskar Singer <oskarsinger@gmail.com>
2017-11-06 18:19:08 -0500

    Enhanced Gaussian sketcher

--- theline/sketching/gaussian.py
+++ theline/sketching/gaussian.py
@@ -2,12 +2,16 @@ import numpy as np
 
 class GaussianSketcher:
 
-    def __init__(self, n, m):
+    def __init__(self, n, m=None):
 
         self.n = n
+
+        if m is None:
+            m = int(0.1 * self.n)
+
         self.m = m
 
-        self.S = np.random.randn(n, m)
+        self.S = np.random.randn(self.n, self.m)
 
     def get_sketched(self, A):
 
