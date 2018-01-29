8992a589d834d07054e55a61554ced062e3c2e66
oskar <oskarsinger@gmail.com>
2018-01-29 01:02:36 -0500

    Rmd unnecessary param

--- theline/utils/rotation.py
+++ theline/utils/rotation.py
@@ -3,12 +3,12 @@ import numpy as np
 from theline.svd import get_svd_power
 from theline.utils import get_multi_dot
 
-def get_rotation(dim, angle, P, P_inv=None):
+def get_rotation(angle, P, P_inv=None):
 
     if P_inv is None:
         P_inv = get_svd_power(P, -1)
 
-    A = np.eye(dim)
+    A = np.eye(P.shape[0])
 
     A[0,0] = np.cos(angle)
     A[1,1] = np.cos(angle)
